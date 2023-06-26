0b99cb0fae2b84518d67f99f2db384fcfa7d1509
Sattocliff <her.gonzalez.andres@gmail.com>
2023-06-26 01:45:33 -0400

    segundo committ

--- peliculas/urls.py
+++ peliculas/urls.py
@@ -7,7 +7,9 @@ urlpatterns=[
     path('index', views.index, name='index'),
     path('crud', views.crud, name='crud'),
     path('peliculasAdd', views.peliculasAdd, name='peliculasAdd'),
-    path('peliculas_del/<str:pk>', views.peliculas_del, name='peliculas_del')
+    path('peliculas_del/<str:pk>', views.peliculas_del, name='peliculas_del'),
+    path('peliculas_findEdit/<str:pk>', views.peliculas_findEdit, name='peliculas_findEdit'),
+    path('peliculasUpdate', views.peliculasUpdate, name='peliculasUpdate')
 
 
 ]
--- peliculas/views.py
+++ peliculas/views.py
@@ -50,4 +50,45 @@ def peliculas_del(request,pk):
         peliculas= Pelicula.objects.all()
         context={'peliculas': peliculas, 'mensaje':mensaje}
         return render(request, 'peliculas/peliculas_list.html', context)
-        
+    
+
+def peliculas_findEdit(request,pk):
+
+    if pk != "":
+        pelicula = Pelicula.objects.get(nombre_pelicula=pk)
+        genero = Genero.objects.all()
+        director = Director.objects.all()
+
+        print(type(pelicula.id_genero.genero))
+
+        context={'pelicula':pelicula, 'genero':genero}
+        if pelicula:
+            return render(request, 'peliculas/peliculas_edit.html', context)
+        else:
+            context={'mensaje': "Error, esta pelicula no existe..."}
+            return render(request, 'peliculas/peliculas_list.html', context)
+
+
+def peliculasUpdate(request):
+    if request.method == "POST":
+        nombrePelicula=request.POST["nombre_pelicula"]
+        genero=request.POST["genero"]
+        director=request.POST["director"]
+        activo="1"
+        objGenero=Genero.objects.get(id_genero = genero)
+        objDirector=Director.objects.get(id_director = director)
+
+        pelicula = Pelicula()
+        pelicula.nombre_pelicula=nombrePelicula
+        pelicula.id_genero=objGenero
+        pelicula.id_director=objDirector
+        pelicula.activo=1
+        pelicula.save()
+        generos=Genero.objects.all()
+        direc=Director.objects.all()
+        context={'mensaje': "Ok, Datos actualizados...", 'generos':generos, 'direc':direc, 'pelicula': pelicula}
+        return render(request, 'peliculas/peliculas_edit.html', context)
+    else:
+        peliculas = Pelicula.objects.all()
+        context={'peliculas': peliculas}
+        return render(request, 'peliculas/peliculas_list.html', context)
